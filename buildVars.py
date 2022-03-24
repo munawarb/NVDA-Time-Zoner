d852e52a129ad8de41b3afd43807baa928268bfc
Munawar Bijani <munawar.bijani@syncoms.com>
2022-03-24 09:08:47 -0400

    Updated compatibility with NVDA 2022.1

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Announces the time and date in selected timezones. Use the Settings dialog to set timezones you want to hear. After you do this, press NVDA+ALT+UP/DOWN ARROW to cycle through your timezone ring and hear times in your selected timezones."""),
 	# version
-	"addon_version" : "2.00",
+	"addon_version" : "3.00",
 	# Author(s)
 	"addon_author" : u"Munawar Bijani",
 	# URL for the add-on documentation support
@@ -27,9 +27,9 @@ addon_info = {
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion" : "2021.1",
+	"addon_minimumNVDAVersion" : "2022.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2021.1",
+	"addon_lastTestedNVDAVersion" : "2022.1",
 	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
 	"addon_updateChannel" : None,
 }

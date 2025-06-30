caa547a779d0fb863f2c14fd03de1e48eaa727cf
0xDEADBEEF <Munawar.Bijani@syncoms.com>
2025-06-30 09:11:33 -0400

    Updated version

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Announces the time and date in selected timezones. Use the Settings dialog to set timezones you want to hear. After you do this, press NVDA+ALT+UP/DOWN ARROW to cycle through your timezone ring and hear times in your selected timezones."""),
 	# version
-	"addon_version" : "3.03",
+	"addon_version" : "3.04",
 	# Author(s)
 	"addon_author" : u"Munawar Bijani",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion" : "2021.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2024.1",
+	"addon_lastTestedNVDAVersion" : "2025.1.2",
 	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
 	"addon_updateChannel" : None,
 }
